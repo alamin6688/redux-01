1c521fe82cedaea00f6c367749d3e152c04c2b39
alamin6688 <alamin.kzs06@gmail.com>
2025-02-19 23:39:13 +0600

    Navbarr added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+// import ResponsiveNavbar from "./components/layout/Navbar";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <div>
-        <Button variant="destructive">ShadCN Button</Button>
+      <div className="flex max-w-screen-xl p-4">
+        <Navbar/>
+        <Button variant="destructive" className="mb-4">
+          ShadCN Button
+        </Button>
+        <Button variant="secondary" className="mb-4">
+          Secondary Button
+        </Button>
       </div>
     </>
   );
