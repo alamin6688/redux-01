51e023cfd303731a0ea405f41cada6abd1ad20db
alamin6688 <alamin.kzs06@gmail.com>
2025-02-28 00:04:06 +0600

    Modified

--- src/redux/features/counter/task/taskSlice.ts
+++ src/redux/features/counter/task/taskSlice.ts
@@ -1,7 +1,6 @@
 import { RootState } from "@/redux/store";
 import { ITask } from "@/types";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from "uuid";
+import { createSlice, PayloadAction, nanoid } from "@reduxjs/toolkit";
 
 interface InitialState {
   tasks: ITask[];
@@ -13,29 +12,35 @@ const initialState: InitialState = {
   filter: "all",
 };
 
+type DraftTask = Pick<ITask, "title" | "description" | "duedate" | "priority">;
+
+const createTask = (taskData: DraftTask): ITask => {
+  return {
+    id: nanoid(),
+    isCompleted: false,
+    ...taskData,
+  };
+};
+
 const taskSlice = createSlice({
   name: "tasks",
   initialState,
   reducers: {
-    addTask: (state, action: PayloadAction<ITask>) => {
-      const id = uuidv4();
-
-      const taskData = {
-        ...action.payload,
-        id,
-        isCompleted: false,
-      };
-
+    addTask: (state, action: PayloadAction<DraftTask>) => {
+      const taskData = createTask(action.payload);
       state.tasks.push(taskData);
     },
   },
 });
+
 export const selectTasks = (state: RootState) => {
   return state.todo.tasks;
 };
+
 export const selectFilter = (state: RootState) => {
   return state.todo.filter;
 };
+
 export const { addTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
